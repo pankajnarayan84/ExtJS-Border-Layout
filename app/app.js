53243969f1d8c3458c2546f6fd574ce2d852e930
pankajnarayan84 <pankaj.narayan84@gmail.com>
2015-04-14 15:13:22 +0530

    ExtJS Hello World with Border Layout
    ExtJS Hello World with Border Layout

---
+++ app/app.js
@@ -0,0 +1,45 @@
+Ext.application({
+	name: 'PNL',
+	appFolder : 'app',
+	controllers: [
+		'PNLController'
+	],
+	requires: [
+		 'Ext.tab.*',
+		'Ext.window.*',
+		'Ext.tip.*',
+		'Ext.layout.container.Border'
+	],
+	createViewport:	function(notifications){
+		Ext.create('Ext.container.Viewport', {
+			layout: 'border',
+			items: [
+				{
+                    region: 'west',
+                    title: 'Navigation',
+                    width: 200,
+                    split: true,
+                    collapsible: true,
+                    floatable: false
+                }, {
+                    region: 'center',
+                    xtype: 'tabpanel',
+                    items: [{
+                        title: 'Bogus Tab',
+                        html: 'Hello world 1'
+                    }, {
+                        title: 'Another Tab',
+                        html: 'Hello world 2'
+                    }, {
+                        title: 'Closable Tab',
+                        html: 'Hello world 3',
+                        closable: true
+                    }]
+                }
+			]
+		});
+	},
+	launch: function() {
+		this.createViewport();
+	}
+});
